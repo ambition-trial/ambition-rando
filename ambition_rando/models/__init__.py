54b9dc4f7c0a3dd39693e97e074f861c11edbb61
erikvw <ew2789@gmail.com>
2017-12-12 03:01:57 +0200

    minor

--- ambition_rando/models/__init__.py
+++ ambition_rando/models/__init__.py
@@ -1,6 +1,8 @@
+import sys
+
 from django.conf import settings
 
 from .randomization_list import RandomizationList
 
-if settings.APP_NAME == 'ambition_rando':
+if settings.APP_NAME == 'ambition_rando' and 'makemigrations' not in sys.argv:
     from ..tests import models
