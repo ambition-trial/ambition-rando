5c4eb1c89c8f9f3962868b4e878e2876e125425a
erikvw <ew2789@gmail.com>
2019-03-16 14:15:41 -0500

    black

--- ambition_rando/management/commands/import_additional.py
+++ ambition_rando/management/commands/import_additional.py
@@ -13,21 +13,20 @@ class AllocationError(Exception):
 
 def get_assignment(allocation):
     if allocation == 1:
-        return 'control'
+        return "control"
     elif allocation == 2:
-        return 'single_dose'
+        return "single_dose"
     else:
-        raise AllocationError(f'Invalid allocation. Got {allocation}')
+        raise AllocationError(f"Invalid allocation. Got {allocation}")
 
 
 def get_allocation(assignment):
-    if assignment == 'control':
+    if assignment == "control":
         return "1"
-    elif assignment == 'single_dose':
+    elif assignment == "single_dose":
         return "2"
     else:
-        raise AllocationError(
-            f'Invalid assignment. Got {assignment}')
+        raise AllocationError(f"Invalid assignment. Got {assignment}")
 
 
 def new_rows(allocations=None, site_name=None, allocation_map=None):
@@ -45,25 +44,27 @@ def new_rows(allocations=None, site_name=None, allocation_map=None):
     return rows
 
 
-def import_allocations(model=None, allocations=None, site_name=None, allocation_map=None, dry_run=None):
-    rows = new_rows(allocations=allocations, site_name=site_name,
-                    allocation_map=allocation_map)
+def import_allocations(
+    model=None, allocations=None, site_name=None, allocation_map=None, dry_run=None
+):
+    rows = new_rows(
+        allocations=allocations, site_name=site_name, allocation_map=allocation_map
+    )
     model = model or "ambition_rando.randomizationlist"
     randomizationlist_model_cls = django_apps.get_model(model)
-    last_sid = randomizationlist_model_cls.objects.all().order_by('sid').last().sid
+    last_sid = randomizationlist_model_cls.objects.all().order_by("sid").last().sid
     objs = []
     for row in rows:
         row = {k: v.strip() for k, v in row.items()}
         try:
-            obj = randomizationlist_model_cls.objects.get(
-                sid=last_sid + row['sid'])
+            obj = randomizationlist_model_cls.objects.get(sid=last_sid + row["sid"])
         except ObjectDoesNotExist:
             obj = randomizationlist_model_cls(
                 id=uuid.uuid4(),
-                sid=last_sid + row['sid'],
-                assignment=row['assignment'],
-                site_name=row['site'],
-                allocation=get_allocation(row['assignment']),
+                sid=last_sid + row["sid"],
+                assignment=row["assignment"],
+                site_name=row["site"],
+                allocation=get_allocation(row["assignment"]),
             )
             objs.append(obj)
     if dry_run:
@@ -72,27 +73,28 @@ def import_allocations(model=None, allocations=None, site_name=None, allocation_
         randomizationlist_model_cls.objects.bulk_create(objs)
 
 
-def import_additional_sids_from_file(filename=None, model=None, allocations=None, dry_run=None):
+def import_additional_sids_from_file(
+    filename=None, model=None, allocations=None, dry_run=None
+):
     objs = []
     filename = filename or "~/rando_additional.txt"
     model = model or "ambition_rando.randomizationlist"
     randomizationlist_model_cls = django_apps.get_model(model)
-    last_sid = randomizationlist_model_cls.objects.all().order_by('sid').last().sid
+    last_sid = randomizationlist_model_cls.objects.all().order_by("sid").last().sid
 
     with open(os.path.join(os.path.expanduser(filename)), "r") as f:
         reader = csv.DictReader(f)
         for index, row in enumerate(reader, 1):
             row = {k: v.strip() for k, v in row.items()}
             try:
-                obj = randomizationlist_model_cls.objects.get(
-                    sid=last_sid + index)
+                obj = randomizationlist_model_cls.objects.get(sid=last_sid + index)
             except ObjectDoesNotExist:
                 obj = randomizationlist_model_cls(
                     id=uuid.uuid4(),
                     sid=last_sid + index,
-                    assignment=row['assignment'],
-                    site_name=row['site'],
-                    allocation=get_allocation(row['assignment']),
+                    assignment=row["assignment"],
+                    site_name=row["site"],
+                    allocation=get_allocation(row["assignment"]),
                 )
                 objs.append(obj)
     if dry_run:
