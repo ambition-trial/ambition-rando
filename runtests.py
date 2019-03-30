6df3f0eb3a8ebbad0728d9a11e616e641bf6aeb1
erikvw <ew2789@gmail.com>
2019-03-30 07:52:27 -0500

    tests

--- runtests.py
+++ runtests.py
@@ -1,95 +1,48 @@
 #!/usr/bin/env python
 import django
 import logging
+import os
 import sys
 
 from django.conf import settings
 from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
 from os.path import abspath, dirname, join
 
 
-class DisableMigrations:
-
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-base_dir = dirname(abspath(__file__))
 app_name = 'ambition_rando'
+base_dir = dirname(abspath(__file__))
 
-installed_apps = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-    'django_crypto_fields.apps.AppConfig',
-    'django_revision.apps.AppConfig',
-    'edc_registration.apps.AppConfig',
-    'edc_device.apps.AppConfig',
-    'edc_identifier.apps.AppConfig',
-    'edc_protocol.apps.AppConfig',
-    'ambition_rando.apps.EdcFacilityAppConfig',
-    'ambition_rando.apps.AppConfig',
-]
-
-DEFAULT_SETTINGS = dict(
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
     BASE_DIR=base_dir,
-    ALLOWED_HOSTS=['localhost'],
-    # AUTH_USER_MODEL='custom_user.CustomUser',
-    ROOT_URLCONF=f'{app_name}.urls',
-    STATIC_URL='/static/',
-    INSTALLED_APPS=installed_apps,
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-        },
-    },
-    TEMPLATES=[{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ]
-        },
-    }],
-    MIDDLEWARE=[
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        "edc_dashboard.middleware.DashboardMiddleware",
-        "edc_subject_dashboard.middleware.DashboardMiddleware",
-    ],
-
-    LANGUAGE_CODE='en-us',
-    TIME_ZONE='UTC',
-    USE_I18N=True,
-    USE_L10N=True,
-    USE_TZ=True,
-
     APP_NAME=app_name,
-    COUNTRY="botswana",
-    ETC_DIR=join(base_dir, "etc"),
-    HOLIDAY_FILE=join(base_dir, app_name, 'tests', 'holidays.csv'),
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    SUBJECT_CONSENT_MODEL="ambition_subject.subjectconsent",
+    SUBJECT_VISIT_MODEL="ambition_subject.subjectvisit",
+    SUBJECT_REQUISITION_MODEL="ambition_subject.subjectrequisition",
     RANDOMIZATION_LIST_PATH=join(
-        base_dir, app_name, 'tests', 'test_randomization_list.csv'),
-    SITE_ID=40,
-
-    DEFAULT_FILE_STORAGE='inmemorystorage.InMemoryStorage',
-    MIGRATION_MODULES=DisableMigrations(),
-    PASSWORD_HASHERS=('django.contrib.auth.hashers.MD5PasswordHasher', ),
-)
+        base_dir, app_name, "tests", "test_randomization_list.csv"),
+    INSTALLED_APPS=[
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django.contrib.sites',
+        'django_crypto_fields.apps.AppConfig',
+        'django_revision.apps.AppConfig',
+        'edc_registration.apps.AppConfig',
+        'edc_device.apps.AppConfig',
+        'edc_identifier.apps.AppConfig',
+        'edc_protocol.apps.AppConfig',
+        'ambition_rando.apps.EdcFacilityAppConfig',
+        'ambition_rando.apps.AppConfig',
+    ],
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+).settings
 
 
 def main():
